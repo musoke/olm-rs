3c806173a38311dc567adade1a54f9eb61a3ddad
Nathan Musoke <nathan.musoke@gmail.com>
2018-01-20 10:44:28 +1300

    hello_world_example: Switch to futures-await
    futures-await is so much more ergonomic.
    
    Based on https://github.com/ruma/ruma-client/blob/master/examples/hello_world_await.rs

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,8 +1,11 @@
-// Based on https://github.com/jplatte/ruma-client/blob/master/examples/hello_world.rs
+// Based on https://github.com/ruma/ruma-client/blob/master/examples/hello_world_await.rs
 #![feature(conservative_impl_trait)]
+#![feature(generators)]
+#![feature(proc_macro)]
 #![feature(try_from)]
 
-extern crate futures;
+extern crate futures_await as futures;
+extern crate olm;
 extern crate ruma_client;
 extern crate ruma_events;
 extern crate ruma_identifiers;
@@ -10,98 +13,105 @@ extern crate tokio_core;
 extern crate url;
 
 use std::convert::TryFrom;
+use std::env;
+use std::process::exit;
 
-use futures::Future;
+use futures::prelude::*;
 use ruma_client::Client;
 use ruma_client::api::r0;
 use ruma_client::api::unstable;
 use ruma_events::EventType;
 use ruma_events::room::message::{MessageEventContent, MessageType, TextMessageEventContent};
 use ruma_identifiers::RoomAliasId;
-use tokio_core::reactor::{Core as TokioCore, Handle as TokioHandle};
+use tokio_core::reactor::{Core, Handle};
 use url::Url;
 
 use std::collections::HashMap;
-
-// from https://stackoverflow.com/a/43992218/1592377
-#[macro_export]
-macro_rules! clone {
-    (@param _) => ( _ );
-    (@param $x:ident) => ( $x );
-    ($($n:ident),+ => move || $body:expr) => (
-        {
-            $( let $n = $n.clone(); )+
-            move || $body
-        }
-    );
-    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
-        {
-            $( let $n = $n.clone(); )+
-            move |$(clone!(@param $p),)+| $body
-        }
-    );
-}
+use olm::device::LocalDevice;
 
 fn hello_world(
-    tokio_handle: &TokioHandle,
+    tokio_handle: &Handle,
     homeserver_url: Url,
+    room: String,
 ) -> impl Future<Item = (), Error = ruma_client::Error> + 'static {
     let client = Client::https(tokio_handle, homeserver_url, None).unwrap();
 
-    let mut one_time_keys = HashMap::new();
-    one_time_keys.insert("curve25519:AAAAAQ".to_owned(), "/qyvZvwjiTxGdGU0RCguDCLeR+nmsb3FfNG3/Ve4vU8".to_owned());
+    async_block! {
+        let session = await!(client.register_guest())?;
 
-    client.register_guest().and_then(clone!(client => move |_| {
+        let my_dev = LocalDevice::init(session.user_id().clone()).unwrap();
+        let dev_keys = my_dev.olm_acount_identity_keys();
+
         // No keys yet, so this should return an empty list
-        unstable::keys::upload::call(client, unstable::keys::upload::Request {
+        let response = await!(unstable::keys::upload::call(client.clone(), unstable::keys::upload::Request {
             device_keys: None,
             one_time_keys: None,
-        }).map(|response| {
-             println!("Before uploading one-time keys: {:?}", response);
-             ()
-        })
-    })).and_then(clone!(client => move |_| {
+        }));
+        println!("Before uploading one-time keys: {:?}", response);
+
+        // A faked one-time key for now.  Will generate an actual one later.
+        let mut one_time_keys = HashMap::new();
+        one_time_keys.insert(
+            "curve25519:AAAAAQ".to_owned(),
+            "/qyvZvwjiTxGdGU0RCguDCLeR+nmsb3FfNG3/Ve4vU8".to_owned(),
+        );
         // Upload some keys, should now get a non-zero response...
-        unstable::keys::upload::call(client, unstable::keys::upload::Request {
-            device_keys: None,
+        let response = await!(unstable::keys::upload::call(client.clone(), unstable::keys::upload::Request {
+            device_keys: Some(dev_keys),
             one_time_keys: Some(one_time_keys),
-        }).map(|response| {
-             println!("After uploading one-time keys: {:?}", response);
-             ()
-        })
-    })).and_then(clone!(client => move |_| {
-        r0::alias::get_alias::call(client.clone(), r0::alias::get_alias::Request {
-            room_alias: RoomAliasId::try_from("#test-e2e:matrix.org").unwrap(),
-        }).and_then(clone!(client => move |response| {
-            let room_id = response.room_id;
+        }));
+        println!("After uploading one-time keys: {:?}", response);
 
-            // Join room for testing e2e
-            r0::membership::join_room_by_id::call(
-                client.clone(),
-                r0::membership::join_room_by_id::Request {
-                    room_id: room_id.clone(),
-                    third_party_signed: None,
-            }).and_then(move |_| {
-            // Send message to room
-                r0::send::send_message_event::call(client, r0::send::send_message_event::Request {
-                    room_id: room_id,
-                    event_type: EventType::RoomMessage,
-                    txn_id: "1".to_owned(),
-                    data: MessageEventContent::Text(TextMessageEventContent {
-                        body: "Hello World!".to_owned(),
-                        msgtype: MessageType::Text,
-                    }),
-                })
-            })
 
-        }))
-    })).map(|_| ())
+        // let response = await!(r0::alias::get_alias::call(
+        //     client.clone(),
+        //     r0::alias::get_alias::Request {
+        //         room_alias: RoomAliasId::try_from(&room[..]).unwrap(),
+        //     }
+        // ))?;
+
+        // let room_id = response.room_id;
+
+        // await!(r0::membership::join_room_by_id::call(
+        //     client.clone(),
+        //     r0::membership::join_room_by_id::Request {
+        //         room_id: room_id.clone(),
+        //         third_party_signed: None,
+        //     }
+        // ))?;
+
+        // await!(r0::send::send_message_event::call(
+        //     client.clone(),
+        //     r0::send::send_message_event::Request {
+        //         room_id: room_id,
+        //         event_type: EventType::RoomMessage,
+        //         txn_id: "1".to_owned(),
+        //         data: MessageEventContent::Text(TextMessageEventContent {
+        //             body: "Hello World!".to_owned(),
+        //             msgtype: MessageType::Text,
+        //         }),
+        //     }
+        // ))?;
+
+        Ok(())
+    }
 }
 
 fn main() {
-    let mut core = TokioCore::new().unwrap();
-    let handle = core.handle();
-    let server = Url::parse("https://matrix.org/").unwrap();
+    let (homeserver_url, room) = match (env::args().nth(1), env::args().nth(2)) {
+        (Some(a), Some(b)) => (a, b),
+        _ => {
+            eprintln!(
+                "Usage: {} <homeserver_url> <room>",
+                env::args().next().unwrap()
+            );
+            exit(1)
+        }
+    };
 
-    core.run(hello_world(&handle, server)).unwrap();
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let server = Url::parse(&homeserver_url).unwrap();
+
+    core.run(hello_world(&handle, server, room)).unwrap();
 }
