fabe8e1a05fa48f5aa47ee7f75f5a2d13ec93c0b
Nathan Musoke <nathan.musoke@gmail.com>
2018-01-24 20:25:23 +1300

    refactor: Pull DeviceId into e2e-types crate
    DeviceId needs to be separated so that it can be used by
    one of olm-rs's dependencies, ruma-client-api.

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -9,47 +9,13 @@ use signing_key::SigningKey;
 use serde_json::value::Value;
 use ruma_signatures;
 use ruma_signatures::Signature;
+use e2e_types::device::DeviceId;
 
 use ruma_identifiers::UserId;
 
 #[derive(Fail, Debug)]
 pub enum DeviceError {
     #[fail(display = "failed to create key")] KeyGenerationError,
-}
-
-#[derive(Debug, Clone)]
-pub struct DeviceId {
-    // TODO: Any requirements on format? Spec just says string; most examples seem to be ~10 upper
-    // case letters
-    id: String,
-}
-
-impl fmt::Display for DeviceId {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.id)
-    }
-}
-
-impl<S> From<S> for DeviceId
-where
-    S: Into<String>,
-{
-    /// # Examples
-    ///
-    /// ```
-    /// let d = olm::device::DeviceId::from("DEVID");
-    /// let s: String = d.to_string();
-    /// ```
-    fn from(s: S) -> DeviceId {
-        DeviceId { id: s.into() }
-    }
-}
-
-// TODO: Rewrite with std::convert
-impl DeviceId {
-    pub fn to_string(&self) -> String {
-        self.id.clone()
-    }
 }
 
 pub struct LocalDevice {
